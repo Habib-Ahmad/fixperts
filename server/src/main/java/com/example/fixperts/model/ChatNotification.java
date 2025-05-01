78329fb645460c0d000bd17d535c2cf77d8f7db5
Bouchene-Mehdi <mehdi.bouchene03@gmail.com>
2025-05-01 17:12:13 +0100

    added booking

--- server/src/main/java/com/example/fixperts/model/ChatNotification.java
+++ server/src/main/java/com/example/fixperts/model/ChatNotification.java
@@ -1,15 +1,15 @@
-package com.example.fixperts.model;
-
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class ChatNotification {
-    private String id;
-    private String senderId;
-    private String receiverId;
-    private String content;
-}
+//package com.example.fixperts.model;
+//
+//import lombok.*;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//public class ChatNotification {
+//    private String id;
+//    private String senderId;
+//    private String receiverId;
+//    private String content;
+//}
